051dce4471ab9cc90e72c62d6d829e75a54657fd
jesusgil01 <39202049+jesusgil01@users.noreply.github.com>
2019-09-27 00:31:22 -0700

    Agregando paquete de consultas

---
+++ src/Consultas/ConsultaSQL.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Consultas;
+
+import Validator.NewUserInfo;
+
+/**
+ *
+ * @author jesus
+ */
+public class ConsultaSQL {
+
+    public String insertTienda(String nombre, String direccion, String telefono, String encargadoid, String ventas) {
+        return "INSERT INTO \"Tiendas\"(\n"
+                + "	nombre, direccion, telefono, encargadoid, ventas)\n"
+                + "	VALUES ('" + nombre + "', '" + direccion + "', '" + telefono + "', " + encargadoid + ", " + ventas + ");";
+    }
+
+    public String consultaTiendas() {
+        return "SELECT *\n"
+                + "FROM dbds413.tiendas;";
+    }
+
+    public String ventasTodasSucursales() {
+        return "SELECT [tiendas`.`id],\n"
+                + "    [tiendas`.`nombre],\n"
+                + "    [tiendas`.`ventas]\n"
+                + "FROM dbds413.tiendas;";
+    }
+
+    public String ventasDeSucursal(String sucursal) {
+        return "SELECT [tiendas`.`id],\n"
+                + "    [tiendas`.`nombre],\n"
+                + "    [tiendas`.`ventas]\n"
+                + "FROM dbds413.tiendas\n"
+                + "WHERE [tiendas`.`nombre] = '" + sucursal + "';";
+    }
+
+    public String insertUsuario(NewUserInfo user) {
+        return "INSERT INTO dbds413.usuarios\n"
+                + "([nombre], [apellidos], [fechanaci], [genero],\n"
+                + "[curp], [rfc], [estadocivil], [telefono], [email],\n"
+                + "[rol], [username], [password], [id_tienda], [salario],\n"
+                + "[direccion])\n"
+                + "VALUES\n"
+                + "('" + user.getNombre() + "', '" + user.getApellido() + "', '" + user.getFechanaci() + "', '" + user.getGenero() + "',\n"
+                + "'" + user.getCurp() + "', '" + user.getRfc() + "', '" + user.getEstadocivil() + "', '" + user.getTelefono() + "', '" + user.getEmail() + "', \n"
+                + "'" + user.getRol() + "', '" + user.getUsername() + "', '" + user.getPassword() + "', " + user.getId_tienda() + ", '" + user.getSalario() + "',\n"
+                + "'" + user.getDireccion() + "');";
+    }
+
+    public String usuariosTotales() {
+        return "SELECT *\n"
+                + "FROM dbds413.usuarios;";
+    }
+
+    public String usuariosPorNombre(String nombre, String apellido) {
+        return "SELECT *\n"
+                + "FROM dbds413.usuarios\n"
+                + "WHERE  [usuarios`.`nombre] = '" + nombre + "' "
+                + "AND [usuarios`.`apellidos] = '" + apellido + "';";
+    }
+
+    public String usuariosSucursal(String nombreSucursal) {
+        return "SELECT *\n"
+                + "FROM dbds413.usuarios\n"
+                + "INNER JOIN `dbds413`.`tiendas`\n"
+                + "ON usuarios.id_tienda = tiendas.id\n"
+                + "WHERE tiendas.nombre = '"+nombreSucursal+"';";
+    }
+
+}
