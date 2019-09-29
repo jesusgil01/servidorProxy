ee91f19222c66e382b5030c16ec0886de3101bae
jesusgil01 <39202049+jesusgil01@users.noreply.github.com>
2019-09-29 16:46:14 -0700

    Corrigiendo consultas

--- src/Consultas/ConsultasPostgreSQL.java
+++ src/Consultas/ConsultasPostgreSQL.java
@@ -1,57 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Consultas;
 
 import Validator.NewUserInfo;
 
-/**
- *
- * @author jesus
- */
 public class ConsultasPostgreSQL {
 
     public String login(String usuario, String password, String rol) {
-        return "SELECT usuarios.id  FROM dbds413.usuarios\n"
-                + "WHERE usuarios.username = '"+usuario+"' "
-                + "AND usuarios.password = '"+password+"' "
-                + "AND usuarios.rol = '"+rol+"';";
+        return "SELECT id\n"
+                + "	FROM public.\"Usuarios\"\n"
+                + "    WHERE username = '" + usuario + "'\n"
+                + "    AND password = '" + password + "' \n"
+                + "    AND rol = '" + rol + "';";
     }
 
     public String insertTienda(String nombre, String direccion, String telefono, String encargadoid, String ventas) {
         return "INSERT INTO public.\"Tiendas\"(\n"
-                + "	nombre, direccion, telefono, encargadoid, ventas)\n"
-                + "	VALUES ('" + nombre + "', '" + direccion + "', '" + telefono + "', " + encargadoid + ", " + ventas + ");";
+                + "nombre, direccion, "
+                + "telefono, encargadoid, ventas)\n"
+                + "	VALUES ('" + nombre + "', '" + direccion + "', "
+                + "'" + telefono + "', '" + encargadoid + "', '" + ventas + "');";
     }
 
     public String consultaTiendas() {
         return "SELECT *\n"
-                + "FROM dbds413.tiendas;";
+                + "	FROM public.\"Tiendas\";";
     }
 
     public String ventasTodasSucursales() {
-        return "SELECT tiendas.id,\n"
-                + "    tiendas.nombre,\n"
-                + "    tiendas.ventas\n"
-                + "FROM dbds413.tiendas;";
+        return "SELECT id, nombre, ventas\n"
+                + "	FROM public.\"Tiendas\";";
     }
 
     public String ventasDeSucursal(String sucursal) {
-        return "SELECT tiendas.id,\n"
-                + "    tiendas.nombre,\n"
-                + "    tiendas.ventas\n"
-                + "FROM dbds413.tiendas\n"
-                + "WHERE tiendas.nombre = '" + sucursal + "';";
+        return "SELECT id, nombre, ventas\n"
+                + "	FROM public.\"Tiendas\"\n"
+                + "	WHERE nombre = '" + sucursal + "';";
     }
 
     public String insertUsuario(NewUserInfo user) {
-        return "INSERT INTO dbds413.usuarios\n"
-                + "(nombre, apellidos, fechanaci, genero,\n"
-                + "curp, rfc, estadocivil, telefono, email,\n"
-                + "rol, username, password, id_tienda, salario,\n"
-                + "direccion)\n"
+        return "INSERT INTO public.\"Usuarios\"(\n"
+                + "id, nombre, apellidos, fechanaci, genero, curp, rfc, estadocivil, "
+                + "telefono, email, rol, username, password, id_tienda, salario, direccion)"
                 + "VALUES\n"
                 + "('" + user.getNombre() + "', '" + user.getApellido() + "', '" + user.getFechanaci() + "', '" + user.getGenero() + "',\n"
                 + "'" + user.getCurp() + "', '" + user.getRfc() + "', '" + user.getEstadocivil() + "', '" + user.getTelefono() + "', '" + user.getEmail() + "', \n"
@@ -60,16 +49,17 @@ public class ConsultasPostgreSQL {
     }
 
     public String usuariosTotales() {
-        return "SELECT *\n"
-                + "FROM dbds413.usuarios;";
+        return "SELECT * FROM public.\"Usuarios\"";
     }
 
     public String usuariosSucursal(String nombreSucursal) {
-        return "SELECT *\n"
-                + "FROM dbds413.usuarios\n"
-                + "INNER JOIN `dbds413`.`tiendas`\n"
-                + "ON usuarios.id_tienda = tiendas.id\n"
-                + "WHERE tiendas.nombre = '" + nombreSucursal + "';";
+        return "SELECT \"Usuarios\".id, \"Usuarios\".nombre, apellidos, fechanaci, genero, curp, rfc, \n"
+                + "	estadocivil, \"Usuarios\".telefono, email, rol, username, password, \n"
+                + "	id_tienda, salario, \"Usuarios\".direccion\n"
+                + "	FROM \"Usuarios\"\n"
+                + "	INNER JOIN \"Tiendas\"\n"
+                + "	ON id_tienda = \"Tiendas\".id\n"
+                + "	WHERE \"Tiendas\".nombre = '"+nombreSucursal+"'";
     }
 
 }
